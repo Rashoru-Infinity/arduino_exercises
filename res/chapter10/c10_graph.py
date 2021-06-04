a3aa3da6a7c2be4069b2cf45d758fdccf8c3abd8
aoba <65536toaru@gmail.com>
2021-06-05 02:48:55 +0900

    new file:   res/chapter10/Figure_1.png 	new file:   res/chapter10/Figure_2.png 	new file:   res/chapter10/Figure_3.png 	modified:   res/chapter10/c10_data1.csv 	modified:   res/chapter10/c10_data2.csv 	modified:   res/chapter10/c10_data3.csv 	modified:   res/chapter10/c10_data4.csv 	modified:   res/chapter10/c10_data5.csv 	deleted:    res/chapter10/c10_fig1.png 	deleted:    res/chapter10/c10_fig2.png 	modified:   res/chapter10/c10_graph.py 	new file:   res/chapter10/distance.csv 	modified:   src/chapter10/chapter10.ino

--- res/chapter10/c10_graph.py
+++ res/chapter10/c10_graph.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    graph.py                                           :+:      :+:    :+:    #
+#    c10_graph.py                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: al19136 <al19136@shibaura-it.ac.jp>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/04 02:11:58 by al19136           #+#    #+#              #
-#    Updated: 2021/06/04 02:53:09 by al19136          ###   ########.fr        #
+#    Updated: 2021/06/05 02:47:08 by al19136          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def do_processing(label, labelList, varDict):
+def do_processing(label, labelList, varDict, distList):
 	csv = "./c10_" + label + ".csv"
 	print(label + ":")
 	data = pd.read_csv(csv, encoding = 'utf-8')
@@ -24,7 +24,11 @@ def do_processing(label, labelList, varDict):
 	varDict['value'].append(np.var(data['value']))
 	plt.plot(data['ms'], data['value'])
 	labelList.append(label)
-
+	distList['val_ave'].append(np.mean(data['value']))
+ 
+distDict = {}
+distDict['dist'] = pd.read_csv('./distance.csv', encoding = 'utf-8')['dist']
+distDict['val_ave'] = []
 labelList = []
 varDict = {}
 varDict['key_ave'] = []
@@ -32,11 +36,11 @@ varDict['value'] = []
 plt.figure(1)
 plt.grid(True)
 plt.title('Relationship between average light intensity and time')
-do_processing('data1', labelList, varDict)
-do_processing('data2', labelList, varDict)
-do_processing('data3', labelList, varDict)
-do_processing('data4', labelList, varDict)
-do_processing('data5', labelList, varDict)
+do_processing('data1', labelList, varDict, distDict)
+do_processing('data2', labelList, varDict, distDict)
+do_processing('data3', labelList, varDict, distDict)
+do_processing('data4', labelList, varDict, distDict)
+do_processing('data5', labelList, varDict, distDict)
 plt.xlabel('Time[ms]')
 plt.ylabel('value')
 plt.legend(labelList)
@@ -46,6 +50,13 @@ plt.title('Relationship between average light intensity and variance')
 plt.xlabel('Average')
 plt.ylabel('Variance')
 plt.scatter(varDict['key_ave'], varDict['value'])
-plt.plot(varDict['key_ave'], np.poly1d(np.polyfit(varDict['key_ave'], varDict['value'], 3))(varDict['key_ave']))
+plt.plot(varDict['key_ave'], np.poly1d(np.polyfit(varDict['key_ave'], varDict['value'], 2))(varDict['key_ave']))
+plt.figure(3)
+plt.grid(True)
+plt.title('Relationship between average light intensity and distance')
+plt.xlabel('Distance[cm]')
+plt.ylabel('Average light intensity')
+plt.scatter(distDict['dist'], distDict['val_ave'])
+plt.plot(distDict['dist'], np.poly1d(np.polyfit(distDict['dist'], distDict['val_ave'], 2))(distDict['dist']))
 
 plt.show()
--- src/chapter10/chapter10.ino
+++ src/chapter10/chapter10.ino
@@ -2,6 +2,7 @@ int photocellPin = 2; //define photocellsh=2, read the value of voltage.
 int ledPin = 12; //define ledPin12 is the output port of led’s level.
 int val = 0; //define original of val.
 int column = 0;
+unsigned long lastTime = -1;
 
 void setup() {
  pinMode(ledPin, OUTPUT); //set ledPin output
@@ -11,13 +12,19 @@ void setup() {
 }
 void loop() {
  val = analogRead(photocellPin); //get the value from sensor
- if (millis() < 30000 + 1000) {
-   if (millis() < 1000)
+ unsigned long time = millis();
+ if (time < 30000 + 1000) {
+   if (time < 1000 && lastTime == -1) {
      Serial.print(0);
-   else
-     Serial.print(millis() - 1000);
-   Serial.print(",");
-   Serial.println(val);
+     Serial.print(",");
+     Serial.println(val);
+     lastTime = 1000;
+   }else if (time != lastTime) {
+    Serial.print(time - 1000);
+    Serial.print(",");
+    Serial.println(val);
+    lastTime = time;
+   }
  }
  if(val<=512){
 //512=2.5V, if want the sensor be more sensitive, increase the number, or lese low the number.
