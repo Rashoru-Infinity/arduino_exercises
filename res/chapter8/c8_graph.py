b18a5db612f17105980da8bb8ea14180137505e8
aoba <65536toaru@gmail.com>
2021-06-07 23:47:38 +0900

    modified:   res/chapter8/Figure_1.png 	modified:   res/chapter8/Figure_2.png 	new file:   res/chapter8/Figure_3.png 	modified:   res/chapter8/c8_graph.py

--- res/chapter8/c8_graph.py
+++ res/chapter8/c8_graph.py
@@ -6,7 +6,7 @@
 #    By: al19136 <al19136@shibaura-it.ac.jp>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/04 02:11:58 by al19136           #+#    #+#              #
-#    Updated: 2021/06/07 23:00:47 by al19136          ###   ########.fr        #
+#    Updated: 2021/06/07 23:25:34 by al19136          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def do_processing(label, varDict, angle):
+def plot_csv_data(prefix, labelList, label) :
+	csv = "./c8_" + prefix + ".csv"
+	data = pd.read_csv(csv, encoding = 'utf-8')
+	plt.plot(data['ms'], data['value'], alpha = 0.4)
+	labelList.append(label)
+
+def append_variance(label, varDict, angle):
 	csv = "./c8_" + label + ".csv"
 	print(label + ":")
 	data = pd.read_csv(csv, encoding = 'utf-8')
@@ -28,25 +34,40 @@ varDict['var'] = []
 varDictPullDown = {}
 varDictPullDown['angle'] = []
 varDictPullDown['var'] = []
+labelList = []
 plt.figure(figsize = (8, 8))
 plt.figure(1)
 plt.grid(True)
-do_processing('data1', varDict, 90)
-do_processing('data2', varDict, 60)
-do_processing('data3', varDict, 30)
-do_processing('data4', varDict, 0)
+plot_csv_data('data1', labelList, 'angle=90 nomal')
+plot_csv_data('data2', labelList, 'angle=60 nomal')
+plot_csv_data('data3', labelList, 'angle=30 nomal')
+plot_csv_data('data4', labelList, 'angle=0 nomal')
+plot_csv_data('data5', labelList, 'angle=90 pull_down')
+plot_csv_data('data6', labelList, 'angle=60 pull_down')
+plot_csv_data('data7', labelList, 'angle=30 pull_down')
+plot_csv_data('data8', labelList, 'angle=0 pull_down')
+plt.xlabel('Time[ms]')
+plt.ylabel('Value')
+plt.legend(labelList)
+plt.figure(figsize = (8, 8))
+plt.figure(2)
+plt.grid(True)
+append_variance('data1', varDict, 90)
+append_variance('data2', varDict, 60)
+append_variance('data3', varDict, 30)
+append_variance('data4', varDict, 0)
 plt.scatter(varDict['angle'], varDict['var'])
 plt.plot(varDict['angle'], np.poly1d(np.polyfit(varDict['angle'], varDict['var'], 2))(varDict['angle']));
 plt.xlabel('Angle[$deg$]')
 plt.ylabel('Variance[$deg^2$]')
 plt.title('NormalCircuit')
 plt.figure(figsize = (8, 8))
-plt.figure(2)
+plt.figure(3)
 plt.grid(True)
-do_processing('data5', varDictPullDown, 90)
-do_processing('data6', varDictPullDown, 60)
-do_processing('data7', varDictPullDown, 30)
-do_processing('data8', varDictPullDown, 0)
+append_variance('data5', varDictPullDown, 90)
+append_variance('data6', varDictPullDown, 60)
+append_variance('data7', varDictPullDown, 30)
+append_variance('data8', varDictPullDown, 0)
 plt.scatter(varDictPullDown['angle'], varDictPullDown['var'])
 plt.plot(varDictPullDown['angle'], np.poly1d(np.polyfit(varDictPullDown['angle'], varDictPullDown['var'], 2))(varDictPullDown['angle']));
 plt.xlabel('Angle[$deg$]')
