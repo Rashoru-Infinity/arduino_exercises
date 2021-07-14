b5f577660bc0c06a63a420d8532ed44a547f5b66
aoba <65536toaru@gmail.com>
2021-07-14 17:15:25 +0900

    modified:   src/zumo/exer4/exer4.ino

--- src/zumo/exer4/exer4.ino
+++ src/zumo/exer4/exer4.ino
@@ -1,40 +1,74 @@
+#include <Wire.h>
 #include <ZumoShield.h>
-#include <wire.h>
+
+// ライントレースセンサを宣言する
+ZumoReflectanceSensorArray reflectanceSensors;
+ZumoMotors motors;
+int lines = 0;
+bool start = false;
+Pushbutton button(ZUMO_BUTTON);
+
+// センサ値を保存するための配列に使うための変数
+// 今回使用するものはセンサを6つ搭載するので6
 #define NUM_SENSORS 6
 uint16_t sensorValues[NUM_SENSORS];
-ZumoMotors motors;
-ZomoReflecttanceSensorArray reflectanceSensors;
 
-
-bool fin = true;
-Pushbutton button(ZUMO_BUTTON);
 
 void setup()
 {
-  Serial(115200);
+  // シリアルを初期化してボーレートを115200に設定する
+  Serial.begin(115200);
+  // センサを初期化する
   reflectanceSensors.init();
+  delay(1000);
+}
+
+// センサ値をシリアルに送る
+void printReadingsToSerial()
+{
+  char buffer[80];
+  sprintf(buffer, "%4d %4d %4d %4d %4d %4d\n",
+    sensorValues[0],
+    sensorValues[1],
+    sensorValues[2],
+    sensorValues[3],
+    sensorValues[4],
+    sensorValues[5]
+  );
+  Serial.print(buffer);
 }
 
 void loop()
 {
-  if (button.getSingleDebouncedPress())
-    fin = false;
-  if (!fin) {
+  if (start)
     motors.setSpeeds(180, 180);
-    delay(1000);
-    motors.setSpeeds(0, 0);
-    delay(100);
-    motors.setSpeeds(180, -180);
-    delay(2600);
-    motors.setSpeeds(0, 0);
-    delay(100);
-    motors.setSpeeds(-180, 180);
-    delay(2600);
-    motors.setSpeeds(0, 0);
-    delay(100);
-    motors.setSpeeds(-180, -180);
-    delay(2000);
-    motors.setSpeeds(0, 0);
-    fin = true;
+  if (button.getSingleDebouncedPress())
+    start = true;
+  // 最後にセンサを読んだ時間を記録
+  static uint16_t lastSampleTime = 0;
+
+  // 最後にセンサを読み取った時間より100ms以上，時間がたった時に
+  // if文の中に入り，もう一度センサを読み取る
+  // millis() プログラムが起動してからの経過時間
+  if ((uint16_t)(millis() - lastSampleTime) >= 100)
+  {
+    // 最後にセンサを読んだ時間を現在時刻にセット
+    lastSampleTime = millis();
+
+    // ライントレースセンサを読み取る
+    reflectanceSensors.read(sensorValues, QTR_EMITTERS_ON);
+    if (sensorValues[4] >= 600 || sensorValues[5] >= 600) {
+      lines++;
+    }
+    if (lines >= 4) {
+      start = false;
+      motors.setSpeeds(0, 0);
+      if (button.getSingleDebouncedPress()) {
+        start = true;
+        lines = 0;
+      }
+    }
+    // 読み取ったセンサの値をprintする
+    printReadingsToSerial();
   }
 }
